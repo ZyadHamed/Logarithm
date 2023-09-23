9d0af887d157f2168d011e1020466e80366446a5
ZyadHamed <zyadhamedashour@gmail.com>
2023-09-23 03:12:38 +0300

    Added files

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post("/SignUp", async (req, res)=>{
   leaderlastname = req.body["leaderlastname"];
   membertwofirstname = req.body["membertwofirstname"];
   membertwolastname = req.body["membertwolastname"];
-  var checkQuery1 = "SELECT * from Participants WHERE teamname='" + teamname + "';";;
+  var checkQuery1 = "SELECT * from Participants WHERE teamname='" + teamname + "';";
   var checkQuery2 = "SELECT * from Participants WHERE email='" + email + "';";
   var sql = "INSERT INTO Participants (teamname, email, password, leaderfirstname, leaderlastname, memberfirstname, memberlastname) VALUES ('" + teamname + "', '" + email + "', '" + password + "', '" + leaderfirstname + "', '" + leaderlastname + "', '" + membertwofirstname + "', '" + membertwolastname + "');";
   const result1 = await queryAsync(checkQuery1);
@@ -38,9 +38,51 @@ app.post("/SignUp", async (req, res)=>{
   res.send({success:"Account Created Successfully!"});
 });
 
+app.post("/Login", async (req, res)=>{
+  email = req.body["email"];
+  password = req.body["password"];
+  var checkQuery = "SELECT * from Participants WHERE email='" + email + "' AND password='" + password + "';";
+  const result = await queryAsync(checkQuery);
+  if(result.length > 0)
+  {
+    res.send({success: "Account was found!", teamname: result[0].teamname})
+  }
+  else
+  {
+    res.send({err: "No account with such credientals was found!"})
+  }
+});
+
+app.post("/InitCompetition", async (req, res)=>{
+  teamname = req.body["teamname"];
+  var checkQuery = "SELECT * from Grades WHERE teamname='" + teamname + "';";
+  const result = await queryAsync(checkQuery);
+  if(result.length > 0)
+  {
+    if(result[0].totalscore == undefined){
+      res.send({ts: result[0].timestamp});
+    }
+    else
+    {
+      res.send({err: "This account has already finished the competition!"});
+    }
+  }
+  else
+  {
+    var timestamp = Date.now();
+    var query = "INSERT INTO Grades (teamname, timestamp) VALUES ('" + teamname + "', '" + timestamp + "');";
+    await queryAsync(query);
+    res.send({ts: timestamp});
+  }
+  
+});
+
 app.get("/", (req, res)=>{
   res.sendFile(__dirname + "/signup.html");
 });
+
+
+
 app.listen(PORT, ()=>{
     console.log("Server is listening on port " + PORT);
 });
