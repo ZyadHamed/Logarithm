cbddf1bba077fa1f2de7127913dbc0d768979553
ZyadHamed <zyadhamedashour@gmail.com>
2023-09-23 04:47:40 +0300

    Update

--- server.js
+++ server.js
@@ -74,7 +74,116 @@ app.post("/InitCompetition", async (req, res)=>{
     await queryAsync(query);
     res.send({ts: timestamp});
   }
-  
+});
+
+app.post("/SubmitAnswers", async (req, res)=>{
+  var q1ans, q2ans, q3ans, q4ans, q5ans, q6ans, q7ans, q8ans, q9ans, q10ans, q11ans, q12ans, q13ans, q14ans, q15ans, q16ans, q17ans, q18ans, q19ans, q20ans = 0;
+  teamname = req.body["teamname"];
+  q1ans = req.body["q1ans"];
+  q2ans = req.body["q2ans"];
+  q3ans = req.body["q3ans"];
+  q4ans = req.body["q4ans"];
+  q5ans = req.body["q5ans"];
+  q6ans = req.body["q6ans"];
+  q7ans = req.body["q7ans"];
+  q8ans = req.body["q8ans"];
+  q9ans = req.body["q9ans"];
+  q10ans = req.body["q10ans"];
+  q11ans = req.body["q11ans"];
+  q12ans = req.body["q12ans"];
+  q13ans = req.body["q13ans"];
+  q14ans = req.body["q14ans"];
+  q15ans = req.body["q15ans"];
+  q16ans = req.body["q16ans"];
+  q17ans = req.body["q17ans"];
+  q18ans = req.body["q18ans"];
+  q19ans = req.body["q19ans"];
+  q20ans = req.body["q20ans"];
+
+  var totalScore = 0;
+  if(q1ans == 0)
+  {
+    totalScore += 3;
+  }
+  if(q2ans == 503)
+  {
+    totalScore += 3;
+  }
+  if(q3ans == 511)
+  {
+    totalScore += 3;
+  }
+  if(q4ans == 210)
+  {
+    totalScore += 2;
+  }
+  if(q5ans == 944)
+  {
+    totalScore += 4;
+  }
+  if(q6ans == 9)
+  {
+    totalScore += 4;
+  }
+  if(q7ans == 99)
+  {
+    totalScore += 5;
+  }
+  if(q8ans == 2023)
+  {
+    totalScore += 5;
+  }
+  if(q9ans == 1999)
+  {
+    totalScore += 5;
+  }
+  if(q10ans == 2)
+  {
+    totalScore += 5;
+  }
+  if(q11ans == 19)
+  {
+    totalScore += 7;
+  }
+  if(q12ans == 32)
+  {
+    totalScore += 7;
+  }
+  if(q13ans == 1008652)
+  {
+    totalScore += 7;
+  }
+  if(q14ans == 8300)
+  {
+    totalScore += 8;
+  }
+  if(q15ans == 2)
+  {
+    totalScore += 8;
+  }
+  if(q16ans == 784)
+  {
+    totalScore += 8;
+  }
+  if(q17ans == 30)
+  {
+    totalScore += 10;
+  }
+  if(q18ans == 75520)
+  {
+    totalScore += 11;
+  }
+  if(q19ans == 1000)
+  {
+    totalScore += 14;
+  }
+  if(q20ans == 299599)
+  {
+    totalScore += 15;
+  }
+  var query = "UPDATE Grades SET 'q1'='" + q1ans == 0 + "', 'q2'='" + q2ans == 503 + "','q3'='" + q3ans == 511 + "','q4'='" + q4ans == 210 + "','q5'='" + q5ans == 944 + "','q6'='" + q6ans == 9 + "','q7'='" + q7ans == 99 + "','q8'='" + q8ans == 2023 + "','q9'='" + q9ans == 1999 + "','q10'='" + q10ans == 2 + "','q11'='" + q11ans == 19 + "','q12'='" + q12ans == 32 + "','q13'='" + q13ans == 1008652 + "','q14'='" + q14ans == 8300 + "','q15'='" + q15ans == 2 + "','q16'='" + q16ans == 784 + "','q17'='" + q17ans == 30 + "','q18'='" + q18ans == 75520 + "','q19'='" + q19ans == 1000 + "','q20'='" + q20ans == 299599 + "','totalscore'='" + totalScore + "' WHERE teamname='" + teamname + "';";
+  await queryAsync(query);
+  res.send({success: "Solutions Submitted Sucessfully!"});
 });
 
 app.get("/", (req, res)=>{
